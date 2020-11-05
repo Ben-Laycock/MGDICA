2b23914b31f283da087feeb2e320398f3f5edf48
LAYCOCK <v8038402@tu54268.windows.tees.ac.uk>
2020-11-05 15:18:14 +0000

    Project File

---
+++ ICA Project/ICA Project/GameScene.swift
@@ -0,0 +1,21 @@
+//
+//  GameScene.swift
+//  ICA Project
+//
+//  Created by LAYCOCK, BEN (Student) on 05/11/2020.
+//  Copyright © 2020 LAYCOCK, BEN (Student). All rights reserved.
+//
+
+import SpriteKit
+import GameplayKit
+
+class GameScene: SKScene
+{
+    
+    override func update(_ currentTime: TimeInterval)
+    {
+        // Code called each frame before rendering
+        
+    }
+    
+}
