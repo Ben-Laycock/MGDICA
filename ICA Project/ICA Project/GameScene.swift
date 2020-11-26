2c78ae13630057765d377c13b3626ec568ff480b
LAYCOCK <v8038402@tu54270.windows.tees.ac.uk>
2020-11-26 08:04:02 -0800

    Virus Class

--- ICA Project/ICA Project/GameScene.swift
+++ ICA Project/ICA Project/GameScene.swift
@@ -10,12 +10,45 @@ import SpriteKit
 import AVFoundation
 import GameplayKit
 
+
+class Virus : SKSpriteNode
+{
+    
+    var health : Int
+    
+    init(texture: SKTexture!, color: UIColor, size: CGSize, health: Int)
+    {
+        
+        self.health = health
+        
+        super.init(texture: texture, color: color, size: size)
+        
+    }
+    
+    required init?(coder aDecoder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+}
+
+
 class GameScene: SKScene
 {
+    
+    let w = UIScreen.main.bounds.width
+    let h = UIScreen.main.bounds.height
+    
+    var virusTexture = SKTexture(imageNamed: "Virus")
+    var v = Virus(texture: SKTexture(imageNamed: "Virus"), color: UIColor.clear, size: SKTexture(imageNamed: "Virus").size(), health: 1)
+    
+    
     var audioPlayer1 = AVAudioPlayer()
     
     override func didMove(to view: SKView)
     {
+        
+        v.position = CGPoint(x: w/2, y: h/2)
+        addChild(v)
         
         let sound = Bundle.main.path(forResource: "Sounds/Beep.mp3", ofType: nil)
         
