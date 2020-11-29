055f7582067a0c70429afc271d862faac3106ee2
LAYCOCK <v8038402@tu54266.windows.tees.ac.uk>
2020-11-29 11:49:26 +0000

    Multiple Scenes

---
+++ ICA Project/ICA Project/MainMenuScene.swift
@@ -0,0 +1,79 @@
+//
+//  MainMenuScene.swift
+//  ICA Project
+//
+//  Created by LAYCOCK, BEN (Student) on 29/11/2020.
+//  Copyright © 2020 LAYCOCK, BEN (Student). All rights reserved.
+//
+
+import SpriteKit
+import AVFoundation
+import GameplayKit
+
+
+class StartScene: SKScene
+{
+    
+    let w = UIScreen.main.bounds.width
+    let h = UIScreen.main.bounds.height
+    
+    var virusTexture = SKTexture(imageNamed: "Virus")
+    var v = Virus(texture: SKTexture(imageNamed: "Virus"), color: UIColor.clear, size: SKTexture(imageNamed: "Virus").size(), health: 1)
+    
+    
+    var audioPlayer1 = AVAudioPlayer()
+    
+    override func didMove(to view: SKView)
+    {
+        
+        v.position = CGPoint(x: w/2, y: h/2)
+        addChild(v)
+        
+        let sound = Bundle.main.path(forResource: "Sounds/Beep.mp3", ofType: nil)
+        
+        do
+        {
+            audioPlayer1 = try AVAudioPlayer(contentsOf: URL(fileURLWithPath: sound!))
+        } catch
+        {
+            print(error)
+        }
+    
+    }
+    
+    
+    override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?)
+    {
+        
+        //let sound = Bundle.main.path(forResource: "Sounds/Beep.mp3", ofType: nil)
+        let playSound = SKAction.playSoundFileNamed("Sounds/Beep.mp3", waitForCompletion: true)
+        self.run(playSound)
+        
+    }
+    
+    override func touchesEnded(_ touches: Set<UITouch>, with event: UIEvent?)
+    {
+        audioPlayer1.play()
+    }
+    
+    override func touchesCancelled(_ touches: Set<UITouch>, with event: UIEvent?)
+    {
+        
+    }
+    
+    
+    func printHelloMessage()
+    {
+        print("Hello Message")
+    }
+    
+    
+    override func update(_ currentTime: TimeInterval)
+    {
+        
+        // Code called each frame before rendering
+        
+        
+    }
+    
+}
